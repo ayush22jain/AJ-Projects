9e619a5ce1f9299a86627a24ccfb3517036c3c7f
ayush22jain <aydj10000@gmail.com>
2022-05-17 17:58:07 -0400

    changed to accomodate list

--- CircleAlgorithm.c
+++ CircleAlgorithm.c
@@ -13,29 +13,46 @@
 // and from there you go clockwise/anticlockwise till you cover all the points.
 // Repeat process for every layer around same location if not in same line
 
-int slicecapacity = 0;
-int slicetracker = 0;
-int layertracker = 0;
+int slicecapacity, slicetracker, layertracker = 0;
+int n = 10000;
 
 typedef struct infocache {
     int slice;
     int layer;
     int pointnumber;
+    int coordinates;
 } ic;
 
-void assign(ic *pointinfo){
+void assign(int input[n]){
+
+    int i, j, temp;
+    struct ic mycaches[n];
+
+    //use bubble sort on input array
+    for (i = 0; i < n; i++){
+      for (j = i + 1; j < n; j++){
+         if (input[i] > input[j]){
+            a = input[i];
+            input[i] = input[j];
+            input[j] = a;
+         }
+      }
+   }
+
+    for(i = 0; i < n; i++){
+    //reset slice capacity and slice number if needed
     if(slicecapacity >= MAX_POINTS){
         slicecapacity = 0;
         slicetracker++;
     }
-
-    if(slicetracker >= 625){
-        slicetracker = 0;
-        layertracker++;
+    mycaches[i] -> pointnumber = i;
+    mycaches[i] -> slice = slicetracker;
+    mycaches[i] -> coordinates = input[i]
+    slicecapacity++;
     }
-
-    pointinfo -> slice = slicetracker;
-    pointinfo -> layer = layertracker;
-    
     return;
+}
+
+int main(int argc, char* argv[]){
+    return 0;
 }
